09677460bbcd005d901d3d3ccb7eaeb56968d692
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-31 14:18:13 -0800

    create form

--- js/compose.js
+++ js/compose.js
@@ -8,7 +8,38 @@ module.exports = {
   },
   renderForm() {
     let draft = MessageStore.getMessageDraft();
-    
+    let html = `
+    <p class='new-message-header'>'New Message'</p>
+    <form class='componse-form'>
+      <input
+      placeholder='Recipient'
+      name='to'
+      type='text'
+      value="${}">
+
+      <input
+      placeholder="Subject"
+      name="subject"
+      type="text"
+      value="${draft.subject}">
+
+      <textarea
+      name="body"
+      rows=20
+      >
+      ${draft.body}
+      </textarea>
+
+      <button
+      type="submit"
+      class="btn btn-primary submit-message"
+      >
+      Send
+      </button>
+    </form>
+    `
+
+    return html;
   }
 
 }
